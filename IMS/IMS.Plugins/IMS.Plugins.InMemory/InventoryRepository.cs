e2d89797d4b5b6b8acf1dc2b2ff620249629e535
Home lab builder <NoPlan422@gmail.com>
2025-12-22 22:50:06 -0500

    inventory IMS app started

---
+++ IMS/IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.Plugins.InMemory
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        private List<Inventory> _inventories;
+
+        public InventoryRepository()
+        {
+            _inventories = new List<Inventory>()
+            {
+                new Inventory{ InventoryId = 1, InventoryName = "Bike Seat", Price = 2.00, Quantity = 10 },
+                new Inventory{ InventoryId = 2, InventoryName = "Bike Body", Price = 15.00, Quantity = 10 },
+                new Inventory{ InventoryId = 3, InventoryName = "Bike Wheels", Price = 8.00, Quantity = 20 },
+                new Inventory{ InventoryId = 4, InventoryName = "Bike Pedals", Price = 1.00, Quantity = 20 },
+            };
+        }
+
+        public async Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return await Task.FromResult(_inventories);
+            }
+            return _inventories.Where(item=> item.InventoryName.Contains(name, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
